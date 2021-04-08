3e8c73c59608f31b839071b129849036e2230f72
Alex DeMarco <demarcoalexandria5@gmail.com>
2021-04-07 17:25:23 -0700

    color code

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 var currentDateEl = $("#currentDay");
+var timeColorEl = $("#time-block");
 
 function todayDate() {
     var thisDay = moment().format("MMM DD, YYYY");
     currentDateEl.text("Today's Date: " + thisDay);
 }
 todayDate();
+
+function colorCode () {
+    var currentHour = moment().hour();
+}
