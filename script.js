c5ed7a71a565f062eb81e0c6e3f77c6e9af374a3
Alex DeMarco <demarcoalexandria5@gmail.com>
2021-04-07 22:21:48 -0700

    color coding works now

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 var currentDateEl = $("#currentDay");
-var timeColorEl = $("#time-block");
+var timeblocksEl = $(".time-block");
 
 function todayDate() {
     var thisDay = moment().format("MMM DD, YYYY");
     currentDateEl.text("Today's Date: " + thisDay);
+    timeblocksEl.each(function() {
+        var currentHour = moment().hours()
+        if(currentHour === $(this).data().time) {
+            $(this).children("textarea").addClass("present");
+        } else if(currentHour > $(this).data().time) {
+            $(this).children("textarea").addClass("past"); 
+        } else {
+            $(this).children("textarea").addClass("future");
+        }
+        
+    })
 }
 todayDate();
 
-function colorCode () {
-    var currentHour = moment().hour();
-}
+
